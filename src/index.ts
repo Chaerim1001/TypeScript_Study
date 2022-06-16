209ae991d30ed6f0f5a41709f827e2b69027664a
ParkChaerim <qkrdbsk28@naver.com>
2022-06-17 00:56:21 +0900

    JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-// declaration file은 자바스크립트 코드의 모양을 타입스크립트에 설명해주는 파일이다. -> .d.ts 파일
-
-import { init, exit } from "myPackage"
-
-init({
-    url: "true"
-})
-
-exit(1)
+import { init, exit } from "./myPackage"
