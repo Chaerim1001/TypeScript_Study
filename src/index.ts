21e716dae03f8de4594b23354ec43891c1f9927e
ParkChaerim <qkrdbsk28@naver.com>
2022-06-18 22:16:04 +0900

    블록체인 완성

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,40 @@ class Block implements BlockShape {
      
      static calculateHash(prevHash: string, height: number, data: string) {
          const toHash = `${prevHash}${height}${data}`;
-         return crypto.createHash("sha356").update(toHash).digest("hex");
+         return crypto.createHash("sha256").update(toHash).digest("hex");
 
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+
+    constructor(){
+        this.blocks = [];
+    }
+
+    private getPrevHash(){
+        if(this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+
+    public addBlock(data: string){
+        const newBlock = new Block(
+            this.getPrevHash(),
+            this.blocks.length + 1,
+            data
+        );
+        this.blocks.push(newBlock);
+    }
+
+    public getBlocks(){
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+blockchain.addBlock('first one');
+blockchain.addBlock('second one');
+blockchain.addBlock('third one');
+
+console.log(blockchain.getBlocks());
